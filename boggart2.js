5aa5a416bd77bd180d2622e47b84b9982cfe0b20
nadyyne <mxnadyx@gmail.com>
2020-02-10 11:10:41 -0500

    desc . . .. .

--- boggart2.js
+++ boggart2.js
@@ -13,22 +13,23 @@ process.stdout.write('> ');
 process.stdin.on('data', run_repl);
 
 let Boggart = class {
-    // functions get applied to arguments
+    // <functions> get applied to <arguments>
     // both of these are stacks:
-    // the last value put on <functions> is the next one
-    // called on the last value put on <args>.
-    // we handle the fact that this can "return" to some
-    // other function and some other arguments, by pushing
-    // those return values back onto the respective stacks.
-    // but we'll just write "calls" and "adds to the stack"
-    // respectively to describe how a function returns.
+    // the last value put on <functions> is called on
+    // the last value put on <args>, and both are removed
+    // from their respective stacks.
+    // we handle the fact that a function can return to some
+    // other functions and some other arguments by pushing
+    // those return values onto the appropriate stacks.
+    // but we'll just write "calls" to say it adds the functions,
+    // and "adds to the stack" to say it adds to the arguments.
     functions = [];
     args = [];
     // our desired fundamental functions are:
-    // -[X] => []         | drop
-    // .[X] => [[X]]      | wrap
-    // :[X][Y] => [YX]    | chain
-    // *[X] => X[X]       | mirror
+    // -[X]  =>  []         | drop
+    // .[X]  =>  [[X]]      | wrap
+    // :[X][Y]  =>  [YX]    | chain
+    // *[X]  =>  X[X]       | mirror
     // we read this as drop(X), wrap(X), chain(X,Y), and mirror(X).
     // functions are applied to whatever follows. so "*.S" reads as "mirror(wrap(S))".
     // [X] means that X is put on the stack. thats why A[X][Y] = A([X]([Y]())) = A(X,Y)
@@ -42,30 +43,74 @@ let Boggart = class {
     //   another way to understand mirror is that it simply chameleons as whatever
     //   the current function on top of the stack is.
     //
-    // then, there is the syntax level. lets say at the syntax level,
+    // then, there is the syntax function. lets say at the syntax level,
     // the program "ABC" usually looks like [A][B][C], or "ABC" --> [A][B][C]. then:
-    // @XY --> X[Y]     | spiral
+    // @XY  -->  X[Y]     | spiral
     // spiral lets us use any of the above functions "on the program".
     // in other words, every function call is treated as value to be manipulated.
     //
     // some examples of this:
-    // @.XP --> .[X]P => [[X]]P     (adds the function X as a first-class value)
-    // @-XP --> -[X]P => P          (forgets that we had written X)
-    // @*XP --> *[X]P => X[X]P      (calls X on itself and the rest of the program)
-    // @:XYP --> :[X][Y]P => [Y X]P (replaces two functions with a single function)
+    // @.XP  -->  .[X]P => [[X]]P     (adds the function X as a first-class value)
+    // @-XP  -->  -[X]P => P          (forgets that we had written X)
+    // @*XP  -->  *[X]P => X[X]P      (calls X on itself and the rest of the program)
+    // @:XYP  -->  :[X][Y]P => [Y X]P (replaces two functions with a single function)
     //
     // for example, normally ABC[X] means A(B(C(X))) ... "do C, then do B, then do A"
-    // but @:@:ABC[X] means C(B(A((X))) ... "do A, then do B, then do C"
+    // but @:@: ABC[X] means C(B(A((X))) ... "do A, then do B, then do C"
     // we write:
-    // @:@:ABC --> ::[A][B][C] => [C B A] --> CBA
+    // @:@: ABC  -->  ::[A][B][C]  -->  CBA
+    // or even:
+    // @@: :: ABC  =>  @[::]ABC  =>  CBA
     //
-    // another example is creating the identity function, which we can write as "-[-]" or "[]"
+    // its useful to create the identity function, which we can write as "-[.]" or "[]".
     // we have no brackets in the language itself, but using "@" we can write:
-    // -@.- --> [-].[-] => [-][[-]] --> -[-] = []
+    // -@..  =>  -[.]  =>  []
+    // and get it as a value in our code by doing:
+    // @.@: @..-  =>  [-[.]]
+    // or, perhaps more clearly (in the sense that the spirals are semantically grouped):
+    // @@:@: .:. .-  =>  @[.:.].-  =>  [-[.]]
+    // or, by just spiral-wraping each part and chaining them later:
+    // : @.@.. @.-  =>  :[[.]][-]  =>  [-[.]]
+    //
     // we can also make the function that calls its argument:
-    // *:@.-[X]S ----> *:[-][X]S => *[X-]S => X-[X-]S => XS
-    // or, as a first-class value:
-    // @.@:@:@.-:* --> .::.[-][:][*] --> [*:[-]]
+    // *:@.- [X]  =>  *:[-][X]  =>  *[X-]  =>  X-[X-]  =>  X
+    // we can wrap it, like before:
+    // @.@:@: @.-:*  -->  .::.[-][:][*]  -->  [*:[-]]
+    // in the cleaner style:
+    // @@:@:@: .::. -:*  =>  @[.::.] -:*  =>  [*:[-]]
+    // or, in the more direct style:
+    // :: @.@.- @.: @.*  =>  ::[[-]][:][*]  =>  [*:[-]]
+    //
+    // for duplicating a value, we can use a similar method:
+    // *::@.- .*. [X]  =>  *::[-].[X][[X]]  =>  *[[X][X]-]  =>  [X][X]
+    // we can also swap two values now:
+    // *:@.-: *:@.@:-. .. [X][Y]  =>  *:[-]: *:[.-][[[X]]] [Y]
+    //     =>  *:[-]:[[X]].[Y] => [Y][X]
+    // and skip values to call a function:
+    // *::@.- *::@.- *:@.@:-. ... [X][Y]  => *::[-] *::[-] *:[-.][[[[X]]]][Y]
+    //     => *::[-][Y][[X]]  =>  [X]Y
+    // or call a single function twice:
+    // ** :*. :@.- [X]  =>  **[[X-]X-]  =>  XX
+    //
+    // we can even make the special function known as the y combinator
+    // *: @.@:@:@:@:@:@:@:@: .*.@.-::*.: [X]
+    //     =>  *: [:.*::[-].*.] [X]
+    //     =>  X :.*::[-].*. [X :.*::[-].*.]
+    //     =>  X [X :.*::[-].*. [X :.*::[-].*.]]
+    // u can see that if X = [*:[-]], it will be called in an infinite loop.
+    //
+    // at this point u might notice that this language is not very pretty :)
+    // i hope ur having fun tho.
+    //
+    // completeness of the language should be obvious, but can be "concisely"
+    // shown by defining the I, K, and S combinators:
+    // I[X] = -@..[X] => [X]
+    // K[X][Y] = *::@.-@.-.[X][Y] => *[[X]--][Y] => [X]
+    // S[X][Y][Z] = *:@.- *: *:@.-: *:@.@:-. .. *::@.- *::@.- *:@.@:-. ...
+    //      @.@:@:@:@:@:@:@:@:@:@:@:@:@:@:@:@:@:@:@:@:@:@:@:@:@:@:@:@:@:@:
+    //      .. @.@:-. :* .@.-: .*. @.-::* @:*: ... @.@:-. :* @.-::* @.-::*
+    // a shorter version of S is very much welcome.
+    //
     parse () { }
     exec () { }
 }
